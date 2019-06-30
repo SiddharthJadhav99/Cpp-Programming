6cfd7a5acb14af19004ce5c0f5a91f3d78f28bea
SiddharthJadhav99 <33030614+SiddharthJadhav99@users.noreply.github.com>
2019-06-30 20:01:22 +0530

    Use of manipulators in the Library <iomanip>

---
+++ Manipulators.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout<<"Using End...."<<endl;
+    cout<<"I Need Some "<<setw(10)<<"s"<<setw(10)<<"p"<<setw(10)<<"a"<<setw(10)<<"c"<<setw(10)<<"e";
+}
