eb9791c880dca76e8c2b0ff4eb517fbfcdf6e879
SiddharthJadhav99 <33030614+SiddharthJadhav99@users.noreply.github.com>
2019-07-31 19:03:48 +0530

    Exclude 6 & 9

---
+++ 6 & 9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        if(i==6||i==9)
+        {
+            continue;
+        }
+       cout<<i<<endl;
+    }
+    return 0;
+}
