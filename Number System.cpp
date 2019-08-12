b460e8dd493bb8141472a5325ad599b35a2bd846
SiddharthJadhav99 <33030614+SiddharthJadhav99@users.noreply.github.com>
2019-08-12 13:20:42 +0530

    Conversions of Number System (with Interface)

--- Number System.cpp
+++ Number System.cpp
@@ -1,5 +1,67 @@
 #include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
+#include <math.h>
+#include <string>
 using namespace std;
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+int ansbox()
+{
+    int rows,cols;
+    for(rows=15;rows<=19;rows++)
+    {
+        gotoxy(50,rows);
+        cout<<"#";
+    }
+   for(cols=50;cols<=69;cols++)
+    {
+        gotoxy(cols,15);
+        cout<<"#";
+    }
+    for(rows=15;rows<=19;rows++)
+    {
+        gotoxy(69,rows);
+        cout<<"#";
+    }
+     for(cols=50;cols<=69;cols++)
+    {
+        gotoxy(cols,19);
+        cout<<"#";
+    }
+
+}
+int design()
+{
+     int rows,cols;
+
+    for(rows=0;rows<=29;rows++)
+    {
+        gotoxy(0,rows);
+        cout<<"*";
+    }
+   for(cols=0;cols<=118;cols++)
+    {
+        gotoxy(cols,0);
+        cout<<"*";
+    }
+    for(rows=0;rows<=29;rows++)
+    {
+        gotoxy(118,rows);
+        cout<<"*";
+    }
+     for(cols=0;cols<=118;cols++)
+    {
+        gotoxy(cols,29);
+        cout<<"*";
+    }
+}
 
 int decToBin(int n)
 {
@@ -53,32 +115,240 @@ int decToHex(int n)
 
      return a[25];
 }
+int binToDec(int n)
+{
+
+
+    int dec_value = 0;
+    int base = 1;
+
+    int temp = n;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+        base = base * 2;
+    }
+
+ return dec_value;
+}
+int octToDec(int n)
+{
+    int dec_value = 0;
+    int base = 1;
+
+    int temp = n;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+        base = base * 8;
+    }
+
+ return dec_value;
+}
+int hexToDec(char val[])
+{
+	int len = strlen(val);
+
+
+	int base = 1;
+
+	int dec_val = 0;
+
+
+	for (int i=len-1; i>=0; i--)
+	{
+
+		if ((val[i]>='0') && (val[i]<='9') )
+		{
+			dec_val += (val[i] - 48)*base;
+
+
+			base = base * 16;
+		}
+
+
+		else if (val[i]>='A' && val[i]<='F')
+		{
+			dec_val += (val[i] - 55)*base;
+
+
+			base = base*16;
+		}
+	}
+
+	return dec_val;
+}
+int binToOct(int n)
+{
+    int dec,oct;
+    dec=binToDec(n);
+    oct=decToOct(dec);
+    return oct;
+}
+int binToHex(int n)
+{
+    int dec,hex;
+    dec=binToDec(n);
+    hex=decToHex(dec);
+    return hex;
+
+}
+int octToBin(int n)
+{
+    int dec,bin;
+    dec=octToDec(n);
+    bin=decToBin(dec);
+    return bin;
+}
+int octToHex(int n)
+{
+    int dec,hex;
+    dec=octToDec(n);
+    hex=decToHex(dec);
+    return hex;
+
+}
+int hexToBin(char val[])
+{
+    int dec,bin;
+    dec=hexToDec(val);
+    bin=decToBin(dec);
+    return bin;
+}
+int hexToOct(char val[])
+{
+    int dec,oct;
+    dec=hexToDec(val);
+    oct=decToOct(dec);
+    return oct;
+}
+
 
 
 int main()
 {
+    design();
     int oper;
-    int dec;
+    int num;
     int ans;
-    cout<<"Enter the decimal value "<<endl;
-    cin>>dec;
-    cout<<"What conversion would you like to perform ? \n 1. Decimal to Binary \n 2. Decimal to Octal \n 3. Decimal to Hexadecimal \n "<<endl;
+    int j,b,o,h;
+    char val[100];
+    gotoxy(41,0);
+    cout<<" NUMBER SYSTEM CONVERTER - NexAltar ";
+    gotoxy(5,3);
+    cout<<" What conversion would you like to perform ? \n"<<endl;
+    gotoxy(5,5);
+    cout<<" 1. Decimal to Binary"<<setw(27)<<"4. Binary To Decimal"<<setw(27)<<"7. Binary to Octal"<<setw(28)<<"10. Octal to Hexa\n"<<endl;
+    gotoxy(5,7);
+    cout<<" 2. Decimal to Octal"<<setw(27)<<"5. Octal to Decimal"<<setw(27)<<"8. Binary to Hexa"<<setw(32)<<"11. Hexa to Decimal \n"<<endl;
+    gotoxy(5,9);
+    cout<<" 3. Decimal to Hexa "<<setw(26)<<"6. Hexa to Decimal"<<setw(29)<<"9. Octal to Binary"<<setw(29)<<"12. Hexa to Octal\n"<<endl;
+    gotoxy(5,13);
+    cout<<" Enter your Choice ===>  ";
     cin>>oper;
-    cout<<endl;
-     cout<<"The Conversion of "<<dec<<" in Binary is "<<endl;
+    gotoxy(5,15);
+    cout<<" Enter the Value ===>   ";
+
+    if(oper==5 || oper==11 || oper==12)
+    {
+        cin>>val;
+    }
+    else
+    {
+        cin>>num;
+    }
+    gotoxy(5,17);
+    cout<<" The Conversion is ===> "<<endl;
+    b=binToDec(num);
+    o=octToDec(num);
+    h=hexToDec(val);
     switch(oper)
     {
-         case 1 : decToBin(dec);
+         case 1 :
+             ansbox();
+            gotoxy(58,17);
+             decToBin(num);
          cout<<endl;
          break;
 
-        case 2 :decToOct(dec);
+        case 2 :
+            ansbox();
+            gotoxy(58,17);
+            decToOct(num);
         cout<<endl;
         break;
 
-        case 3 :decToHex(dec);
+        case 3 :
+            ansbox();
+            gotoxy(58,17);
+            decToHex(num);
+        cout<<endl;
+        break;
+
+        case 4 :
+            ansbox();
+            gotoxy(58,17);
+            cout<<b<<endl;
+        break;
+
+        case 5 :
+            ansbox();
+            gotoxy(58,17);
+             cout<<o<<endl;
+        break;
+
+        case 6 :
+            ansbox();
+            gotoxy(58,17);
+             cout<<h<<endl;
+        break;
+
+        case 7:
+            ansbox();
+            gotoxy(58,17);
+            binToOct(num);
+            cout<<endl;
+            break;
+
+        case 8 :
+            ansbox();
+            gotoxy(58,17);
+            binToHex(num);
+        cout<<endl;
+        break;
+
+        case 9 :
+            ansbox();
+            gotoxy(58,17);
+            octToBin(num);
+        cout<<endl;
+        break;
+
+        case 10 :
+            ansbox();
+            gotoxy(58,17);
+            octToHex(num);
+        cout<<endl;
+        break;
+
+        case 11 :
+            ansbox();
+            gotoxy(58,17);
+            hexToBin(val);
+        cout<<endl;
+        break;
+
+        case 12 :
+            ansbox();
+            gotoxy(58,17);
+            hexToOct(val);
         cout<<endl;
         break;
     }
+    gotoxy(5,26);
     return 0;
 }
